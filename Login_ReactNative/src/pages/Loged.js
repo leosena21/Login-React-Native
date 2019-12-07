858efc74723b5cbcdcfa42a4a48f9e32c5184302
Leonardo Sena <leeosena21@gmail.com>
2019-12-07 00:28:29 -0300

    Ajuste tela Loged e inserção do Button do facebook para logout

--- Login_ReactNative/src/pages/Loged.js
+++ Login_ReactNative/src/pages/Loged.js
@@ -1,11 +1,59 @@
 import React, { Component } from 'react';
 
-import { View } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+
+import { LoginButton, AccessToken } from 'react-native-fbsdk';
 
 // import { Container } from './styles';
 
 export default class pages extends Component {
   render() {
-    return <View />;
+    return (
+     <View style={styles.container}>
+       <Text style={styles.Text}>Login realizado com sucesso</Text>
+
+       <LoginButton
+          onLoginFinished={
+            (error, result) => {
+              if (error) {
+                console.log("login has error: " + result.error);
+              } else if (result.isCancelled) {
+                console.log("login is cancelled.");
+              } else {            
+                AccessToken.getCurrentAccessToken().then(
+                  (data) => {
+                    console.log(data.accessToken.toString());
+                    this.props.navigation.navigate('Loged');
+                }
+                )
+              }
+            }
+          }
+          onLogoutFinished={
+            () => console.log("logout.") | this.props.navigation.navigate('Main')
+            }/>
+
+     </View> 
+    )
   }
 }
+
+const styles = StyleSheet.create({  
+  
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    marginTop: 100,
+    margin:10,
+    padding:20,
+    marginHorizontal: 16,
+  },
+
+  Text: {
+    alignItems: 'center',
+    alignSelf: 'center',
+    justifyContent: 'center',
+    marginTop: 200,
+    fontSize: 20,
+  },
+});
--- Login_ReactNative/src/pages/Main.js
+++ Login_ReactNative/src/pages/Main.js
@@ -47,10 +47,6 @@ export default class Main extends Component {
   }
 }
 
-function handleLogin(){
-  
-}
-
 const styles = StyleSheet.create({                                                                                                      
   container: {
     flex: 1,
