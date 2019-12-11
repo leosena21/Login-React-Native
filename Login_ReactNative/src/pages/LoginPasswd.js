776ac13b1266a6aa3f57f2e80406432f21b63606
Leonardo Sena <leeosena21@gmail.com>
2019-12-10 23:11:14 -0300

    Solicitando php para acesso ao banco de dados

--- Login_ReactNative/src/pages/LoginPasswd.js
+++ Login_ReactNative/src/pages/LoginPasswd.js
@@ -1,26 +1,83 @@
 import React, { Component } from 'react';
 
-import {View, StyleSheet, Text, TouchableOpacity} from 'react-native';
+import {View, StyleSheet, Text, TouchableOpacity, Alert} from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
 
 // import { Container } from './styles';
 
 export default class LoginPasswd extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      userEmail: "",
+      posts: {}
+    };
+  };
+
+  upd = () => {
+
+      fetch('https://planetaaguaba.com/Testephp/teste.php',{
+			method:'POST',
+			header:{
+				'Accept': 'application/json',
+				'Content-type': 'application/json'
+			},
+			body:JSON.stringify({
+        // we will pass our input data to server
+        
+				email_user: this.state.userEmail
+			})
+			
+		})
+		.then((response) => response.json())
+    .then(res => {
+      this.setState({
+          ...this.state, // Not required but just a heads up on using mutation
+          posts: res //Não é necessario isso aqui, mas é valido para guardar informações futuras
+      })
+
+      if(res.retorno=='CADASTRADO'){
+        this.popup(this.props.navigation.push('Loged'));
+      }
+      else{
+        this.popup();
+      }
+  })
+  .catch((error => {
+      console.error(error);
+  }));
+  }
+
+  popup(test){
+    // Works on both Android and iOS
+    Alert.alert(
+      'Informação',
+      this.state.posts.retorno,
+      [
+        {text: 'OK', onPress: () => test},
+      ],
+      {cancelable: false},
+    );
+  }
+    
   render(){
     return (
       <View style={styles.container}>
-        <Text style={styles.title}> Realize Login</Text>
+        <Text style={styles.title}> {this.state.posts.retorno}</Text>
         <TextInput 
           style={styles.input}
           placeholder="Informe o usuario"
+          onChangeText={userEmail => this.setState({userEmail})}
         />
 
         <TextInput
           style={styles.input}
           placeholder="Informe o senha"
+          onChangeText={userPassword => this.setState({userPassword})}
         />
 
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity onPress={() => this.upd()} style={styles.button}>
           <Text style={styles.text} >Realizar Login</Text>
         </TouchableOpacity>
       </View>
